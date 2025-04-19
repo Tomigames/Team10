8cd3fc8b21a86e84f194bcd62811fa1f76c7b59d
Amulya Rayabhagi <114515586+AmulyaRayabhagi05@users.noreply.github.com>
2025-04-19 17:53:25 -0500

    finished parts of phase 5

---
+++ Phase5/my-frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import GradeViewApp from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GradeViewApp />
+  </React.StrictMode>
+);
